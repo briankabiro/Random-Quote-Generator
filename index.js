e8ad23ae37ba23411eb6206d8fa9ab0d9c0a7655
BrianLee <briankigondu@gmail.com>
2017-02-04 21:16:04 +0300

    Can get data from api

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 /*Get json data of quotes, connect with socket.io, on-button click, read the next one */
+var http = require('http');
+
+var options = {
+	host:'www.quotesondesign.com',
+	path:'/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+};
+
+http.request(options, function(response){
+	var str = '';
+	response.on('data', function(chunk){
+		str += chunk;
+	});
+	response.on('end',function(){
+		console.log(str);
+	});
+})
